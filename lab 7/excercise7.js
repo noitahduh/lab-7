55c1308c04fb575c455878fa40eb59dd62278cdf
noitahduh <ainhoaordonez995@gmail.com>
2024-09-13 18:23:49 -0500

    Lab 7

---
+++ lab 7/excercise7.js
@@ -0,0 +1,21 @@
+/*
+Ejercicio 7: Validar contraseñas
+Crea una función llamada validarContrasena(contrasena) que verifique si una contraseña cumple con las siguientes reglas:
+
+Debe tener al menos 8 caracteres.
+Debe contener al menos un número.
+Si cumple con ambas, retornar "Contraseña válida", de lo contrario "Contraseña no válida".
+*/
+function validarContrasena(contrasena) {
+    if (contrasena.length >= 8 && /\d/.test(contrasena)) {
+        return "Contraseña válida";
+    } else {
+        return "Contraseña no válida";
+    }
+}
+
+console.log("-----------------------------------")
+console.log("EJERCICIO 7")
+console.log("-----------------------------------")
+console.log(validarContrasena("pass1234")); // "Contraseña válida"
+console.log(validarContrasena("password")); // "Contraseña no válida"
